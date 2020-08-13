8c4c4997fcd29b83a2a9b271998931f3b05c3826
Mike Manchee <mikemanchee@gmail.com>
2020-08-13 09:55:43 -0700

    debug js on jsVoting
    co-authored-by: Alma Frankenstein <a.q.frankenstein@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,15 @@ $(document).ready(function() {
   if (age >= 18) {
     $('#old-enough').show();
   } else {
+    let timeTill = 18 - age;
+    
+    if (age === 1) {
+      timeTill = timeTill + " year";
+    } else {
+      timeTill = timeTill + " years";
+    }
+    
+    $("#time-till").text(timeTill);
     $('#not-old-enough').show();
   }
 });
